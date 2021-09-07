2328120e99cb752d6b81ff336c00308a08ca72af
RaniKamna <kamna.a139@gmail.com>
2021-09-07 22:07:08 +0530

    youtube app update

--- YouTube/script.js
+++ YouTube/script.js
@@ -1,15 +1,14 @@
 let div = document.getElementById('video_div')
 async function searchVideos() {
+    div.innerHTML = null;
     let video = document.getElementById('video').value;
 
     let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${video}&type=video&key=AIzaSyBjmgRNkeCgc7GK4TTJUV0DcaDzwXBC23c&maxResults=20`);
     let data = await res.json();
-    console.log(data)
 
     for ({
         id: { videoId }
     } of data.items) {
-        //console.log(videoId)
         let video_frame = document.createElement('iframe')
         video_frame.src = `https://www.youtube.com/embed/${videoId}`
 
@@ -17,3 +16,17 @@ async function searchVideos() {
         div.appendChild(video_frame)
     }
 }
+
+async function defaultVideo(){
+    let res = await fetch('https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=IN&key=AIzaSyBjmgRNkeCgc7GK4TTJUV0DcaDzwXBC23c&maxResults=16');
+    let data = await res.json();
+
+    for ({id} of data.items) {
+        let video_frame = document.createElement('iframe')
+        video_frame.src = `https://www.youtube.com/embed/${id}`
+
+        video_frame.allow = `fullscreen`
+        div.appendChild(video_frame)
+    }
+}
+defaultVideo()
